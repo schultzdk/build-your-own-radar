5cfe010f1f98d6d1f2386874542e0b82e8e56407
Kim Bjørn Tiedemann <kbt@schultz.dk>
2018-08-21 14:34:56 +0200

    Added using the csv file in data by default

--- src/util/factory.js
+++ src/util/factory.js
@@ -169,13 +169,7 @@ const GoogleSheetInput = function () {
         var domainName = DomainName(window.location.search.substring(1));
         var queryParams = QueryParams(window.location.search.substring(1));
 
-        if (window.location.href.indexOf('schultz-teknologivision') != -1)
-        {
-            var file = window.location.href + '/' + 'data/Schultz-teknologivision-2018-2020.csv'
-            var sheet = CSVDocument(file);
-            sheet.init().build();
-        }
-        else if (domainName && queryParams.sheetId.endsWith('csv')) {
+        if (domainName && queryParams.sheetId.endsWith('csv')) {
             var sheet = CSVDocument(queryParams.sheetId);
             sheet.init().build();
         }
@@ -184,24 +178,30 @@ const GoogleSheetInput = function () {
             console.log(queryParams.sheetName)
 
             sheet.init().build();
-        } else {
-            var content = d3.select('body')
-                .append('div')
-                .attr('class', 'input-sheet');
-            set_document_title();
-
-            plotLogo(content);
-
-            var bannerText = '<div><h1>Build your own radar</h1><p>Once you\'ve <a href ="https://www.thoughtworks.com/radar/byor">created your Radar</a>, you can use this service' +
-                ' to generate an <br />interactive version of your Technology Radar. Not sure how? <a href ="https://www.thoughtworks.com/radar/how-to-byor">Read this first.</a></p></div>';
-
-            plotBanner(content, bannerText);
-
-            plotForm(content);
-
-            plotFooter(content);
-
+        } 
+        else {
+            var file = window.location.href + '/data/Schultz-teknologivision-2018-2020.csv'
+            var sheet = CSVDocument(file);
+            sheet.init().build();
         }
+        // else {
+        //     var content = d3.select('body')
+        //         .append('div')
+        //         .attr('class', 'input-sheet');
+        //     set_document_title();
+
+        //     plotLogo(content);
+
+        //     var bannerText = '<div><h1>Build your own radar</h1><p>Once you\'ve <a href ="https://www.thoughtworks.com/radar/byor">created your Radar</a>, you can use this service' +
+        //         ' to generate an <br />interactive version of your Technology Radar. Not sure how? <a href ="https://www.thoughtworks.com/radar/how-to-byor">Read this first.</a></p></div>';
+
+        //     plotBanner(content, bannerText);
+
+        //     plotForm(content);
+
+        //     plotFooter(content);
+
+        //}
     };
 
     return self;
